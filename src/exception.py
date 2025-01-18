7506e6bcc88197466fbfccccb223b676cba6793d
mansisawantt <mansisawant438@gmail.com>
2025-01-17 22:58:47 -0800

    Hyperparameter training done

--- src/exception.py
+++ src/exception.py
@@ -3,21 +3,18 @@ from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename #How get this search on google custome exception handling in python documentation
-    error_message="Error occured in python Script name [{0}] line number [{1}] error message[{2}]".format(
-
-    file_name,exc_tb.tb_lineno,str(error)   )
+    file_name=exc_tb.tb_frame.f_code.co_filename #how to getthis search on google exception handling doc.
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
-    
-    
 
     
+
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
-  
     
     def __str__(self):
         return self.error_message
